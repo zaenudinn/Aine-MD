94bc9e0e826a5e9ac849e56b1f18ac8881d110df
Riadixdganz <110595666+Riadixdganz@users.noreply.github.com>
2022-09-01 17:52:02 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
+  ['6282199069274'],
   ['62895330379186'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]

c7cdaee2d9b1049864a87bcf0dc7db6f0b25cb0a
zaenudinn <112722681+zaenudinn@users.noreply.github.com>
2022-09-03 07:26:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['6283835317555'],
-  ['6283835317555'],
-  ['6283835317555', 'Owner', true]
+  ['6283143155277'],
+  ['6283143155277'],
+  ['6283143155277', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
